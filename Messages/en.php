e6f0f6e596d167af0a704ab66b53e46f04918d2f
Weblate <noreply@weblate.org>
2023-09-01 15:02:12 +0300

    Update translation files
    Updated by "Cleanup translation files" hook in Weblate.
    
    Translation: MIKOPBX/ModuleTelegramProvider
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduletelegramprovider/

--- Messages/en.php
+++ Messages/en.php
@@ -17,9 +17,7 @@ return [
     'module_telegram_providerUrlGetId' => 'An error occurred while trying to log in',
     'module_telegram_providerReconnect' => 'Get "App api_id" and "App api_hash"',
     'module_telegram_providerCopy' => 'Copy to clipboard',
-    'module_template_AddNewRecord' => 'Add new',
     'module_telegram_providerTextFieldLabel' => 'Text field example',
-    'module_telegram_providerTextAreaFieldLabel' => 'TextArea field example',
     'module_telegram_providerPasswordFieldLabel' => 'Password field example',
     'module_telegram_providerIntegerFieldLabel' => 'Integer field example',
     'module_telegram_providerCheckBoxFieldLabel' => 'CheckBox',
@@ -32,7 +30,5 @@ return [
     'module_telegram_providerUpdateStatus' => 'Update module status',
     'module_telegram_providerError' => 'An error occurred while trying to login',
     'module_telegram_AddNewRecord' => 'Add',
-
     'module_telegram_providerWaitAuth' => 'Waiting for the authorization result...',
-
 ];
