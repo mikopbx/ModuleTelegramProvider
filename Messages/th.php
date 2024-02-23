1690fddc5050031a727b3e9f4b28542c7c5160ed
Weblate <noreply@weblate.org>
2024-02-23 05:56:50 +0300

    Added translation using Weblate (Thai)

---
+++ Messages/th.php
@@ -0,0 +1,94 @@
+<?php
+return [
+    /*
+ * MikoPBX - free phone system for small business
+ * Copyright © 2017-2023 Alexey Portnov and Nikolay Beketov
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+    /**
+ * Copyright (C) MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Nikolay Beketov, 6 2018
+ *
+ */
+    'repModuleTelegramProvider' => '',
+    'mo_ModuleModuleTelegramProvider' => '',
+    'BreadcrumbModuleTelegramProvider' => '',
+    'SubHeaderModuleTelegramProvider' => '',
+    'module_telegram_AddNewRecord' => '',
+    'module_telegram_provider_phone_number' => '',
+    'module_telegram_provider_api_id' => '',
+    'module_telegram_provider_api_hash' => '',
+    'module_telegram_providerUrlGetId' => '',
+    'module_telegram_providerStep2' => '',
+    'module_telegram_providerStep2Title' => '',
+    'module_telegram_providerStep1' => '',
+    'module_telegram_providerStep1Part1' => '',
+    'module_telegram_providerStep1Part2' => '',
+    'module_telegram_providerStep3' => '',
+    'module_telegram_providerStep3Title' => '',
+    'module_telegram_providerStep4' => '',
+    'module_telegram_providerStep4Title' => '',
+    'module_telegram_providerError' => '',
+    'module_telegram_providerReconnect' => '',
+    'module_telegram_providerIntegerFieldLabel' => '',
+    'module_telegram_providerCheckBoxFieldLabel' => '',
+    'module_telegram_providerToggleFieldLabel' => '',
+    'module_telegram_providerDropDownFieldLabel' => '',
+    'module_telegram_providerValidateValueIsEmpty' => '',
+    'module_telegram_providerConnected' => '',
+    'module_telegram_providerCopy' => '',
+    'module_telegram_providerDisconnected' => '',
+    'module_telegram_providerNotAllConnected' => '',
+    'module_telegram_providerUpdateStatus' => '',
+    'module_telegram_providerWaitAuth' => '',
+    'module_telegram_provider_gw' => '',
+    'module_telegram_provider_user' => '',
+    'module_telegram_provider_status_gw_OK' => '',
+    'module_telegram_provider_status_gw_WAIT_START' => '',
+    'module_telegram_provider_status_gw_FAIL' => '',
+    'module_telegram_provider_status_user_OK' => '',
+    'module_telegram_provider_status_user_WAIT_START' => '',
+    'module_telegram_provider_status_user_FAIL' => '',
+    'module_telegram_provider_status_bot_OK' => '',
+    'module_telegram_provider_status_bot_WAIT_START' => '',
+    'module_telegram_provider_status_bot_FAIL' => '',
+    'module_telegram_provider_action_remove' => '',
+    'messengerGetPhoneCode' => '',
+    'messengerGetBotToken' => '',
+    'Confirm this login link on another device:' => '',
+    'Enter authentication password:' => '',
+    'Enter your first name:' => '',
+    'Enter your last name:' => '',
+    'Enter phone number:' => '',
+    'Enter the employee internal number' => '',
+    'Internal number entry form' => '',
+    'Ordering a callback' => '',
+    'MessageNoAnswer' => '',
+    'request a call back' => '',
+    'Identifiers' => '',
+    'module_telegram_provider_bot_token' => '',
+    'MessageTemplates' => '',
+    'CallbackText' => '',
+    'businessCardText' => '',
+    'businessCardSubText' => '',
+    'keyboardText' => '',
+    'keyboardSubText' => '',
+    'autoAnswerText' => '',
+    'autoAnswerTextSubText' => '',
+    'callbackQueueText' => '',
+    'callbackQueueSubText' => '',
+];
